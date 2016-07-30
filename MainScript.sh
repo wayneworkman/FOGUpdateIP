9eae614fef7cdcdd1eb0a66b8a79576128fdfc53
Tom Elliott <tommygunsster@gmail.com>
2016-07-30 12:28:20 -0400

    All finished, just needs testing

--- MainScript.sh
+++ MainScript.sh
@@ -21,6 +21,8 @@ mv=$(command -v mv)
 rm=$(command -v rm)
 date=$(command -v date)
 touch=$(command -v touch)
+dirname=$(command -v dirname)
+basename=$(command -v basename)
 
 #---- Set non-required command paths ----#
 ip=$(command -v ip)
@@ -51,6 +53,8 @@ checkCommand "$echo" "echo" "$log"
 checkCommand "$mv" "mv" "$log"
 checkCommand "$date" "date" "$log"
 checkCommand "$touch" "touch" "$log"
+checkCommand "$dirname" "dirname" "$log"
+checkCommand "$basename" "basename" "$log"
 
 #Record the date.
 NOW=$($date '+%d/%m/%Y %H:%M:%S')
@@ -103,138 +107,134 @@ while [[ -z $IP ]]; do
     sleep 5
 done
 
+# If the IP's match, exit immediately.
+if [[ $ip == $ipaddress ]]; then
+    $echo "The IP address found on $interface matches the IP set in $fogsettings, assuming all is good, exiting." >> $log
+    exit
+fi
 # If the IP from fogsettings doesn't match what the system returned #
 #    Make the change so system can still function #
-if [[ $ip != $ipaddress ]]; then
-    #---- Update the IP Setting ----#
-    $echo "The IP Address for $interface does not match the ipaddress setting in $fogsettings, updating the IP Settings server-wide." >> $log
-    statement1="UPDATE \`globalSettings\` SET \`settingValue\`='$ip' WHERE \`settingKey\` IN ('FOG_TFTP_HOST','FOG_WOL_HOST','FOG_WEB_HOST');"
-    statement2="UPDATE \`nfsGroupMembers\` SET \`ngmHostname\`='$ip' WHERE \'ngmMemberName\`='$storageNode' OR \'ngmHostname\`='$ipaddress';"
-    sqlStatements="$statement1$statement2"
+#---- Update the IP Setting ----#
+$echo "The IP Address for $interface does not match the ipaddress setting in $fogsettings, updating the IP Settings server-wide." >> $log
+statement1="UPDATE \`globalSettings\` SET \`settingValue\`='$ip' WHERE \`settingKey\` IN ('FOG_TFTP_HOST','FOG_WOL_HOST','FOG_WEB_HOST');"
+statement2="UPDATE \`nfsGroupMembers\` SET \`ngmHostname\`='$ip' WHERE \'ngmMemberName\`='$storageNode' OR \'ngmHostname\`='$ipaddress';"
+sqlStatements="$statement1$statement2"
 
-    # Builds proper SQL Statement and runs.
-    # If no user defined, assume root
-    [[ -z $snmysqluser ]] && $snmysqluser='root'
-    # If no host defined, assume localhost/127.0.0.1
-    [[ -z $snmysqlhost ]] && $snmysqlhost='127.0.0.1'
-    # No password set, run statement without pass authentication
-    if [[ -z $snmysqlpass ]]; then
-        $echo "A password was not set in $fogsettings for mysql use" >> $log
-        $mysql -u"$snmysqluser" -e "$sqlStatements" "$database" 2>> $log
-        # Else run with password authentication
-    else
-        $echo "A password was set in $fogsettings for mysql use" >> $log
-        $mysql -u"$snmysqluser" -p"${snmysqlpass}" -e "$sqlStatements" "$database" 2>> $log
-    fi
-
-    #---- Update IP address in file default.ipxe ----#
-    $echo "Updating the IP in $tftpfile" >> $log
-    $sed -i "s|http://\([^/]\+\)/|http://$ip/|" $tftpfile
-    $sed -i "s|http:///|http://$ip/|" $tfptfile
-
-    #---- Check docroot and webroot is set ----#
-    checkFogSettingVars "$docroot" "docroot" "$fogsettings" "$log"
-    checkFogSettingVars "$webroot" "docroot" "$fogsettings" "$log"
-
-    #---- Set config file location and check----#
-    configfile="${docroot}${webroot}lib/fog/config.class.php"
-    checkFilePresence "$configfile" "$log"
-
-    #---- Backup config.class.php ----#
-    $echo "Backing up $configfile" >> $log
-    $cp -f "$configfile" "${configfile}.old"
-
-    #---- Update IP in config.class.php ----#
-    $echo "Updating the IP inside $configfile" >> $log
-    $sed -i "s|\".*\..*\..*\..*\"|\$_SERVER['SERVER_ADDR']|" $configfile
-
-    #---- Update .fogsettings IP ----#
-    $echo "Updating the ipaddress field inside of $fogsettings" >> $log
-    $sed -i "s|ipaddress='.*'|ipaddress='$ip'|g" $fogsettings
-
-    # check if customfogsettings exists, if not, create it.
-    if [[ ! -f $customfogsettings ]]; then
-        $echo "$customfogsettings was not found, creating it." >> $log
-        $touch $customfogsettings
-    fi
-
-    checkFilePresence "$customfogsettings" "$log"
-
-    # Source custom fogsettings
-    . $customfogsettings
-    if [[ -z $dodnsmasq ]]; then
-        $echo "The dodnsmasq setting was not found in $customfogsettings, adding it." >> $log
-        # Add dodnsmasq setting
-        $echo "dodnsmasq='1'" >> $customfogsettings
-    fi
-    if [[ -z $bldnsmasq ]]; then
-        $echo "The bldnsmasq setting was not found in $customfogsettings, adding it." >> $log
-        # Add bldnsmasq
-        $echo "bldnsmasq='1'" >> $customfogsettings
-    fi
-    # Resource both settings files
-    . $fogsettings
-    . $customfogsettings
-
-    # Verify dodnsmasq and bldnsmasq are indeed set
-    checkFogSettingVars "$dodnsmasq" 'dodnsmasq' "$fogsettings or $customfogsettings" "$log"
-    checkFogSettingVars "$bldnsmasq" 'bldnsmasq' "$fogsettings or $customfogsettings" "$log"
-
-    # Build dnsmasq ltsp file
-    if [[ $bldnsmasq -eq 1 ]]; then
-        [[ -z $ltspfile ]] && ltspfile="/etc/dnsmasq.d/ltsp.conf"
-        $echo "bldnsmasq inside $fogsettings or $customfogsettings was set to enabled, recreating $ltspfile" >> $log
-        # Check ltspfile presence
-        [[ ! -f $ltspfile ]] && $touch $ltspfile
-        if [[ -z $bootfilename ]]; then
-            $echo "The bootfilename setting in either $fogsettings or $customfogsettings is not set, setting to undionly.kkpxe" >> $log
-            bootfilename="undionly.kkpxe"
-        fi
-        [[ -z $bootfilepath ]] && bootfilepath="/tftpboot/$bootfilename"
-        bootfileCopy="${bootfilename%.*}.0"
-        if [[ -f $bootfileCopy ]]; then
-            $echo "$bootfileCopy was found, deleting it." >> $log
-            $rm -f $bootfileCopy
-        fi
-        $echo "Copying $bootfilepath to $bootfileCopy for dnsmasq to use." >> $log
-        $cp $bootfilepath $bootfileCopy
-
-        # Creating config file
-        $echo "Recreating $ltspfile for use with dnsmasq." >> $log
-        # backing up ltspfile
-        $mv $ltspfile ${ltspfile}.backup
-        $echo -e "port=0\n \
-            log-dhcp\n \
-            tftp-root=$(dirname $bootfilepath)\n\
-            dhcp-boot=$(basename $bootfileCopy),$ip,$ip\n\
-            dhcp-option=17,$storageLocation\n\
-            dhcp-option=vendor:PXEClient,6,2b\n\
-            dhcp-no-override\n\
-            pxe-prompt=\"Press F8 for boot menu\",60\n\
-            pxe-service=X86PC,\"Boot from network\",$(basename $bootfilepath)\n\
-            pxe-service=X86PC,\"Boot from local hard disk\",0\n\
-            dhcp-range=$ip,proxy" > $ltspfile
-    fi
-    if [[ $dodnsmasq -eq 1 ]]; then
-    fi
-    if [[ "$dodnsmasq" == "1" ]]; then
-        $echo dodnsmasq was set to 1 inside of $customfogsettings - starting it and enabling it to run at boot. >> $log
-        $echo You may manually set this to 0 if you like, and manually stop and disable dnsmasq with these commands: >> $log
-        $echo systemctl disable dnsmasq >> $log
-        $echo systemctl stop dnsmasq >> $log
-        $systemctl enable dnsmasq
-        $systemctl restart dnsmasq
-    else
-        $echo dodnsmasq was set to 0 inside of $customfogsettings - stopping it and disabling it from running at boot. >> $log
-        $echo You may manually set this to 1 if you like, and manually start and enable dnsmasq with these commands: >> $log
-        $echo systemctl enable dnsmasq >> $log
-        $echo systemctl restart dnsmasq >> $log
-        $systemctl disable dnsmasq
-        $systemctl stop dnsmasq
-
-    fi
-
+# Builds proper SQL Statement and runs.
+# If no user defined, assume root
+[[ -z $snmysqluser ]] && $snmysqluser='root'
+# If no host defined, assume localhost/127.0.0.1
+[[ -z $snmysqlhost ]] && $snmysqlhost='127.0.0.1'
+# No password set, run statement without pass authentication
+if [[ -z $snmysqlpass ]]; then
+    $echo "A password was not set in $fogsettings for mysql use" >> $log
+    $mysql -u"$snmysqluser" -e "$sqlStatements" "$database" 2>> $log
+    # Else run with password authentication
 else
-    $echo The IP address found on $interface matches the IP set in $fogsettings, assuming all is good, exiting. >> $log
-    exit
+    $echo "A password was set in $fogsettings for mysql use" >> $log
+    $mysql -u"$snmysqluser" -p"${snmysqlpass}" -e "$sqlStatements" "$database" 2>> $log
+fi
+
+#---- Update IP address in file default.ipxe ----#
+$echo "Updating the IP in $tftpfile" >> $log
+$sed -i "s|http://\([^/]\+\)/|http://$ip/|" $tftpfile
+$sed -i "s|http:///|http://$ip/|" $tfptfile
+
+#---- Check docroot and webroot is set ----#
+checkFogSettingVars "$docroot" "docroot" "$fogsettings" "$log"
+checkFogSettingVars "$webroot" "docroot" "$fogsettings" "$log"
+
+#---- Set config file location and check----#
+configfile="${docroot}${webroot}lib/fog/config.class.php"
+checkFilePresence "$configfile" "$log"
+
+#---- Backup config.class.php ----#
+$echo "Backing up $configfile" >> $log
+$cp -f "$configfile" "${configfile}.old"
+
+#---- Update IP in config.class.php ----#
+$echo "Updating the IP inside $configfile" >> $log
+$sed -i "s|\".*\..*\..*\..*\"|\$_SERVER['SERVER_ADDR']|" $configfile
+
+#---- Update .fogsettings IP ----#
+$echo "Updating the ipaddress field inside of $fogsettings" >> $log
+$sed -i "s|ipaddress='.*'|ipaddress='$ip'|g" $fogsettings
+
+# check if customfogsettings exists, if not, create it.
+if [[ ! -f $customfogsettings ]]; then
+    $echo "$customfogsettings was not found, creating it." >> $log
+    $touch $customfogsettings
+fi
+
+checkFilePresence "$customfogsettings" "$log"
+
+# Source custom fogsettings
+. $customfogsettings
+if [[ -z $dodnsmasq ]]; then
+    $echo "The dodnsmasq setting was not found in $customfogsettings, adding it." >> $log
+    # Add dodnsmasq setting
+    $echo "dodnsmasq='1'" >> $customfogsettings
+fi
+if [[ -z $bldnsmasq ]]; then
+    $echo "The bldnsmasq setting was not found in $customfogsettings, adding it." >> $log
+    # Add bldnsmasq
+    $echo "bldnsmasq='1'" >> $customfogsettings
+fi
+# Resource both settings files
+. $fogsettings
+. $customfogsettings
+
+# Verify dodnsmasq and bldnsmasq are indeed set
+checkFogSettingVars "$dodnsmasq" 'dodnsmasq' "$fogsettings or $customfogsettings" "$log"
+checkFogSettingVars "$bldnsmasq" 'bldnsmasq' "$fogsettings or $customfogsettings" "$log"
+
+# Build dnsmasq ltsp file
+if [[ $bldnsmasq -eq 1 ]]; then
+    [[ -z $ltspfile ]] && ltspfile="/etc/dnsmasq.d/ltsp.conf"
+    $echo "bldnsmasq inside $fogsettings or $customfogsettings was set to enabled, recreating $ltspfile" >> $log
+    # Check ltspfile presence
+    [[ ! -f $ltspfile ]] && $touch $ltspfile
+    if [[ -z $bootfilename ]]; then
+        $echo "The bootfilename setting in either $fogsettings or $customfogsettings is not set, setting to undionly.kkpxe" >> $log
+        bootfilename="undionly.kkpxe"
+    fi
+    [[ -z $bootfilepath ]] && bootfilepath="/tftpboot/$bootfilename"
+    bootfileCopy="${bootfilename%.*}.0"
+    if [[ -f $bootfileCopy ]]; then
+        $echo "$bootfileCopy was found, deleting it." >> $log
+        $rm -f $bootfileCopy
+    fi
+    $echo "Copying $bootfilepath to $bootfileCopy for dnsmasq to use." >> $log
+    $cp $bootfilepath $bootfileCopy
+
+    # Creating config file
+    $echo "Recreating $ltspfile for use with dnsmasq." >> $log
+    # backing up ltspfile
+    $mv $ltspfile ${ltspfile}.backup
+    $echo -e "port=0\n \
+        log-dhcp\n \
+        tftp-root=$($dirname $bootfilepath)\n\
+        dhcp-boot=$($basename $bootfileCopy),$ip,$ip\n\
+        dhcp-option=17,$storageLocation\n\
+        dhcp-option=vendor:PXEClient,6,2b\n\
+        dhcp-no-override\n\
+        pxe-prompt=\"Press F8 for boot menu\",60\n\
+        pxe-service=X86PC,\"Boot from network\",$($basename $bootfilepath)\n\
+        pxe-service=X86PC,\"Boot from local hard disk\",0\n\
+        dhcp-range=$ip,proxy" > $ltspfile
+fi
+if [[ $dodnsmasq -eq 1 ]]; then
+    $echo "dodnsmasq was enabled in either $fogsettings or $customfogsettings - starting dnsmasq/proxyDHCP and enabling to run at boot time." >> $log
+    $echo "You may manually set this to 0 if you like, and manually stop/disable dnsmasq with these commands:" >> $log
+    $echo "systemctl disable dnsmasq" >> $log
+    $echo "systemctl stop dnsmasq" >> $log
+    $systemctl enable dnsmasq
+    $systemctl restart dnsmasq
+else
+    $echo "dodnasmasq was disabled in either $fogsettings or $customfogsettings - stopping dnsmasq/proxyDHCP and disabling boot time startup." >> $log
+    $echo "You may manually set this to 1 if you like, and manually start/enable dnsmasq with these commands:" >>$ log
+    $echo "systemctl enable dnsmasq" >> $log
+    $echo "systemctl restart dnsmasq" >> $log
+    $systemctl disable dnsmasq
+    $systemctl stop dnsmasq
 fi
